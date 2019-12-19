8796d0c8ba540ba08c8a5a1602034e1e2c7655e8
1401338 <eshakhator@gmail.com>
2019-12-19 10:15:01 -0500

    Initial commit

---
+++ app/src/main/java/trap1/khatoresha/pixelstopallettes/MainActivity.java
@@ -0,0 +1,14 @@
+package trap1.khatoresha.pixelstopallettes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
